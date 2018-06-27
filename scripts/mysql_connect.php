e5ba7d4b5ba3152cbe2401a8820518ae5682f58f
Shakti Naik <shakti.vssut@gmail.com>
2018-06-27 23:59:19 +0530

    Index page bug corrected

--- scripts/mysql_connect.php
+++ scripts/mysql_connect.php
@@ -5,7 +5,7 @@ $db_username = "shakguqg";
 $db_password = "power123$";
 $db_name = "shakguqg_data";
 
-$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
 // Check connection
 if ($conn->connect_error) {
